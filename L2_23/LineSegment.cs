a037d1ac9f7744e73673579027d0ac93484f2b37
Nanana <Nanana@DESKTOP-F9S2RI4>
2024-11-01 10:53:14 +0500

    Добавьте файлы проекта.

---
+++ L2_23/LineSegment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2_23
+{
+    internal class LineSegment
+    {
+        double x;
+        double y;
+
+        public LineSegment(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // проверка пересечения отрезков
+        public static bool Intersects(LineSegment segment1, LineSegment segment2)
+        {
+            return !(segment1.y < segment2.x || segment2.y < segment1.x);
+        }
+        // вычисление длины отрезка
+        public static double operator !(LineSegment segment)
+        {
+            return Convert.ToDouble(Convert.ToDecimal(segment.y) - Convert.ToDecimal(segment.x));
+        }
+        // расширение отрезка на 1 вправо и влево
+        public static LineSegment operator ++(LineSegment segment)
+        {
+            return new LineSegment(segment.x - 1, segment.y + 1);
+        }
+        // неявное к int
+        public static implicit operator int(LineSegment segment)
+        {
+            return (int)segment.x;
+        }
+        // явное к double
+        public static explicit operator double(LineSegment segment)
+        {
+            return segment.y;
+        }
+        // левосторонняя операция, уменьшается координата х
+        public static LineSegment operator -(LineSegment segment, int value)
+        {
+            return new LineSegment(segment.x - value, segment.y);
+        }
+        // правосторонняя операция, уменьшается координата y
+        public static LineSegment operator -(int value, LineSegment segment)
+        {
+            return new LineSegment(segment.x, segment.y - value);
+        }
+        public override string ToString()
+        {
+            return $"\n[{x}, {y}]";
+        }
+    }
+}
+
---
+++ L2_23/Program.cs
@@ -0,0 +1,70 @@
+﻿using L2_23;
+using System;
+using System.Security.Cryptography.X509Certificates;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Координаты первого отрезка: ");
+            Console.Write("X: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            if (x1 > y1)
+            {
+                double xy1 = x1;
+                x1 = y1;
+                y1 = xy1;
+            }
+
+            Console.WriteLine("\nКоординаты второго отрезка: ");
+            Console.Write("X: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            if (x2 > y2)
+            {
+                double xy2 = x2;
+                x2 = y2;
+                y2 = xy2;
+            }
+
+            Console.WriteLine($"\n[{x1}, {y1}] [{x2}, {y2}]");
+
+            LineSegment segment1 = new LineSegment(x1, y1);
+            LineSegment segment2 = new LineSegment(x2, y2);
+
+            Console.WriteLine((LineSegment.Intersects(segment1, segment2) ? "Отрезки пересекаются" : "Отрезки не пересекаются"));
+            Console.WriteLine($"Длина отрезка [{x1}, {y1}] = {!segment1}");
+
+
+            LineSegment segment3 = segment1;
+            segment3++;
+            Console.WriteLine($"Расширение отрезка [{x1}, {y1}] -> {segment3.ToString()}");
+
+            // Неявное int
+            Console.WriteLine($"Целая часть X: {(int)segment1}");
+            // Явное double
+            Console.WriteLine($"Координата Y (double): {(double)segment1}");
+
+            Console.Write("Введите значение, на которое уменьшается координата Х: ");
+            int bix = Convert.ToInt32(Console.ReadLine());
+            LineSegment segment4 = segment1 - bix;
+            Console.WriteLine($"После уменьшения X на {bix} -> {segment4.ToString()}");
+
+            Console.Write("Введите значение, на которое уменьшается координата Y: ");
+            int biy = Convert.ToInt32(Console.ReadLine());
+            LineSegment segment5 = biy - segment1;
+            Console.WriteLine($"После уменьшения Y на {biy} -> {segment5.ToString()}");
+        }
+
+        catch
+        {
+            Console.WriteLine("Некорректный ввод");
+        }
+    }
+}
